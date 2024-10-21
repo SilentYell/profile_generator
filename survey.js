cd50c60d375bc1ecfd284c2e7592f4476ed3166e
Nik Hixson <nikilobe@gmail.com>
2024-10-21 15:23:04 -0700

    added all the questions in the assignment including rl.close to exit at the end

--- survey.js
+++ survey.js
@@ -5,7 +5,23 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question("What do you think of Node.js? ", (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
-  rl.close();
+rl.question("What's your name? Nicknames are also acceptable :) ", (name) => {
+  rl.question("What's an activity you like doing? ", (activity) => {
+    rl.question("What do you listen to while doing that? ", (music) => {
+      rl.question("Which meal is your favourite (eg: dinner, brunch, etc.)? ", (meal) => {
+        rl.question("What's your favourite thing to eat for that meal? ", (food) => {
+          rl.question("Which sport is your absolute favourite? ", (sport) => {
+            rl.question("What is your superpower? In a few words, tell us what you are amazing at! ", (superpower) => {
+              console.log(`\nHere's your generated profile:\n
+              ${name} loves ${activity} while listening to ${music}.
+              Their favorite meal is ${meal}, and they love to eat ${food}.
+              They enjoy watching or playing ${sport}, and their superpower is ${superpower}!\n`);
+              
+              rl.close();
+            });
+          });
+        });
+      });
+    });
+  });
 });
